0ec4674a9218f8f72df282c6cb8f2e75e9e3c079
scharla <scharla@purdue.edu>
2024-12-30 22:05:19 -0500

    Completed adding the phi_algorithm implementation

--- Regressor/phi_algorithm.py
+++ Regressor/phi_algorithm.py
@@ -12,13 +12,88 @@ class phiAlg():
         self.ssd = self.dat.ssd
 
     # ========================================================
-    def get_km_dat(self, k) -> km.km_dat:
+    def get_km_dat(self, k: int) -> km.km_dat:
         """ Extract the current and previous data for the time step and store it in cp_dat object """
         return km.km_dat(self.ssd, k)
 
     # ===============================================================================================
-    def phi(self, k):
+    def phi(self, k: int) -> np.ndarray[2, 1]:
         """ phi(k) = [T[k], 1]^T """
-        Tk = self.ssd["T"][k]
-        phi_k = np.matrix([[Tk], [1]])
+        T_k = self.ssd["T"][k]
+        phi_k = np.matrix([[T_k], [1]])
         return phi_k
+
+    # =================================
+    def phi_tau(self, k: int) -> np.ndarray[2, 1]:
+        """ phi_tau(k) = (1/F(k)) phi(k)"""
+        phi_k = self.phi(k)
+        F_k = self.ssd["F"][k]
+        phi_tau_k = (1/F_k) * phi_k
+        return phi_tau_k
+
+    # =======================================
+    def phi_ur(self, k: int) -> np.ndarray[2, 1]:
+        """ phi_ur(k) = u_2(k) * phi_tau(k) """
+        phi_tau_k = self.phi_tau(k)
+        u2_k = self.ssd['u2'][k]
+        phi_ur_k = u2_k * phi_tau_k
+        return phi_ur_k
+
+    # ===========================================
+    def phi_1(self, k: int) -> np.ndarray[2, 1]:
+        """ phi_1(k) = u1(k) * phi_tau(k) """
+        phi_tau_k = self.phi_tau(k)
+        u1_k = self.ssd['u1'][k]
+        phi_1_k = u1_k * phi_tau_k
+        return phi_1_k
+
+    # =========================================
+    def f_phi1(self, k: int) -> float:
+        """ f_phi1 = (uk/um) * (Fm/Fk) * (Tk*Tm +1)/(Tm^2 + 1) """
+        ssd_km = self.get_km_dat(k)
+        u1_frac = ssd_km.u1k/ssd_km.u1m
+        F_frac = ssd_km.Fm/ssd_km.Fk
+        T_frac = (ssd_km.Tk * ssd_km.Tm + 1) / (ssd_km.Tm**2 + 1)
+        f_phi1_k = u1_frac * F_frac * T_frac
+        return f_phi1_k
+
+    # ==============================================================
+    def phi_f1(self, k: int) -> np.ndarray[6, 1]:
+        """ phi_f1(k) = eta(k) * f_phi1(k) * [phi_ur(m); phi(m); u1(m)*phi(m)] """
+        m = k-1
+        eta_k = self.ssd["eta"][k]
+        f_phi1_k = self.f_phi1(k)
+        phi_ur_m = self.phi_ur(m)
+        phi_m = self.phi(m)
+        u1_m = self.ssd['u1'][m]
+        K = eta_k * f_phi1_k
+        M = np.concatenate((phi_ur_m, phi_m, u1_m*phi_m), axis=0)
+        return K*M
+
+    # ===============================================================================
+    def phi_gamma1(self, k: int) -> np.ndarray[2, 1]:
+        """ phi_gamma1(k) = (u1k/Fk)*(u2m/Fm)*phi(k) """
+        phi_k = self.phi(k)
+        km_ssd = self.get_km_dat(k)
+        phi_gamma1_k = (km_ssd.u1k/km_ssd.Fk) * (km_ssd.u2m/km_ssd.Fm) * phi_k
+        return phi_gamma1_k
+
+    # ==========================================================================
+    def phi_nox(self, k: int) -> np.ndarray[8, 1]:
+        """ phi_nox(k) = [-phi_f1(k); phi_gamma1(k)] """
+        phi_f1_k = self.phi_f1(k)
+        phi_gamma1_k = self.phi_gamma1(k)
+        phi_nox_k = np.concatenate((-phi_f1_k, phi_gamma1_k), axis=0)
+        return phi_nox_k
+
+    # ======================================================================
+    def y(self, k: int) -> float:
+        """ y(k) = eta(k) - eta(k-1)*f_phi1(k-1)"""
+        m = k-1
+        eta_k = self.ssd["eta"][k]
+        eta_m = self.ssd["eta"][m]
+        f_phi1_m = self.f_phi1(m)
+        y_k = eta_k - eta_m * f_phi1_m
+        return y_k
+
+    # =============================================
