2fcd37de6c4b122edc0db290984ea17cd4221454
scharla <scharla@purdue.edu>
2025-01-01 22:01:32 -0500

    Started the RLS code

---
+++ RLS/dir_imports.py
@@ -0,0 +1,5 @@
+import sys
+sys.path.append('../DataProcessing')
+sys.path.append('../Regressor')
+import decimate_data as dd
+import phi_algorithm as ph
---
+++ RLS/rlsCode.py
@@ -0,0 +1,11 @@
+import numpy as np
+from dir_imports import  *
+
+
+# =========================
+class rls_run():
+    """ RLS algorithm with all the memoization """
+    # =============================================
+    def __init__(self, dat: dd.decimatedTestData):
+        """ Starts the RLS algorithm """
+        self.phi_alg = ph.phiAlg(dat)
