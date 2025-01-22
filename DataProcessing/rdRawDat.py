Corrected the units for all the states and iinputs. PPM depends on the temperature as it is a function of volume

DataProcessing/rdRawDat.py
@@ -33,27 +33,27 @@ class RawTestData():
         # ======================================================================================
         # Mass flow rate is in g/sec
         F_kgmin = np.array(data.get(('EXHAUST_FLOW', 'kg/min')), dtype=np.float64).flatten()
-        raw_data['F'] = uc.uConv(F_kgmin,Tscr, "kg/min to g/s")        # g/sec
+        raw_data['F'] = F_kgmin  # kg/min  #uc.uConv(F_kgmin,Tscr, "kg/min to g/s")        # g/sec
         # =======================================================================================
         # NOx output is in mol/m^3
         NOx = np.array(data.get(('EXH_CW_NOX_COR_U1', 'PPM')), dtype=np.float64).flatten()
-        raw_data['x1'] = uc.uConv(NOx, Tscr, "ppm to mol/m^3")
+        raw_data['x1'] = uc.uConv(NOx, Tscr, "ppm to 10^-3 mol/m^3")
         # =======================================================================================
         # NH3 output is in mol/m^3
         NH3 = np.array(data.get(('EXH_CW_AMMONIA_MEA', 'ppm')), dtype=np.float64).flatten()
-        raw_data['x2'] = uc.uConv(NH3, Tscr, "ppm to mol/m^3")
+        raw_data['x2'] = uc.uConv(NH3, Tscr, "ppm to 10^-3 mol/m^3")
         # =======================================================================================
         # NOx out measured in mol/m^3
         y1 = np.array(data.get(('V_SCM_PPM_SCR_OUT_NOX', 'ppm')), dtype=np.float64).flatten()
-        raw_data['y1'] = uc.uConv(y1, Tscr, "ppm to mol/m^3")
+        raw_data['y1'] = uc.uConv(y1, Tscr, "ppm to 10^-3 mol/m^3")
         # =======================================================================================
         # NOx input is in mol/m^3
         u1 = np.array(data.get(('ENG_CW_NOX_FTIR_COR_U2', 'PPM')), dtype=np.float64).flatten()
-        raw_data['u1'] = uc.uConv(u1, Tscr, "ppm to mol/m^3")
+        raw_data['u1'] = uc.uConv(u1, Tscr, "ppm to 10^-3 mol/m^3")
         # ========================================================================================
         # Urea injection rate is in ml/sec
         u2 = np.array(data.get(('V_UIM_FLM_ESTUREAINJRATE', 'ml/sec')), dtype=np.float64).flatten()
-        raw_data['u2'] = uc.uConv(u2, Tscr, "ml/s to 10^-3 ml/s")
+        raw_data['u2'] = uc.uConv(u2, Tscr, "ml/s to 10 ml/s")
         # u1_sensor = np.array(Data.get(('EONOX_COMP_VALUE', 'ppm'))).flatten()
         # ======================================================================================================
         return raw_data

DataProcessing/unit_convs.py
@@ -18,14 +18,32 @@ def uConv(x, Tscr, conv_type: str):
     """Unit conversion for the states"""
     match conv_type:
         case "-T0C":
-            return np.array([xi - T0 for xi in x])
+            # print("Setting reference temperature as x 10 + 200 deg-C")
+            return np.array([(xi - T0)/10 for xi in x])
         case "kg/min to g/s":
+            # print("Converting kg/min to g/s")
             return np.array([xi * kgmin2gsec for xi in x])
         case "ppm to 10^-3 mol/m^3":
+            # print("converting ppm (mole fraction) to 10^{-3} mol/m^3")
             return np.array([(xi/(22.4*((273.15+T_scr)/(273.15)))) for (xi, T_scr) in zip(x, Tscr)])
-        case "ml/s to 10^-3 ml/s":
-            return np.array([xi*1e3 for xi in x])
+        case "ml/s to 10 ml/s":
+            # print("Scaling urea-injection to 10^{-3} ml/s")
+            return np.array([xi*10 for xi in x])
         case _: # Default
-            return x
+            raise ValueError("Unknown unit conversion")
 #===
+
+std_units = {"T": r'$\quad [\times 10 + 200 \: ^0C]$',
+             "F": r'$\quad [kg/min]$',
+           "con": r'$\quad [\times 10^{-3} \, mol/m^3]$',
+          "uinj": r'$\quad [\times 10 \, ml/sec]$'}
+
+units = {"T":std_units["T"],
+         "F":std_units["F"],
+         "x1":std_units["con"],
+         "x2":std_units["con"],
+         "u1":std_units["con"],
+         "eta":std_units["con"],
+         "y1":std_units["con"],
+         "u2":std_units["uinj"]}
 