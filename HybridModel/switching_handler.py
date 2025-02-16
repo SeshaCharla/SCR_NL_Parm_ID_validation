51b6e8040e27e505a3a7b0a14322a36abfe3cd43
scharla <scharla@purdue.edu>
2025-02-16 16:05:15 -0500

    Made the switching handler standard and to use narrow partittions by default

--- HybridModel/switching_handler.py
+++ HybridModel/switching_handler.py
@@ -1,21 +1,15 @@
 # Details of the hybrid model switching conditions
-
 T_parts = [-17.45, (-17.45 -6.55)/2,  -6.55, (-6.55 + 2.4)/2, 2.4, (2.4 + 15.5)/2, 15.5]
+# A wider partition
+T_parts_w = [-17.45, -6.55, 2.4, 15.5]
+# No Partitions
+T_parts_n = [-17.45, 15.5]
+# By default T_parts is used. We need to replace with a wider partions or no partition, T_parts needed to be reassigned.
+
 intervals = [(T_parts[i], T_parts[i+1]) for i in range(len(T_parts)-1)]
 Nparts = len(intervals)
 
-# A wider partition
-T_parts_w = [-17.45, -6.55, 2.4, 15.5]
-intervals_w = [(T_parts_w[i], T_parts_w[i+1]) for i in range(len(T_parts_w)-1)]
-Nparts_w = len(intervals_w)
-
-# No partition
-T_parts_n = [-17.45, 15.5]
-intervals_n = [(T_parts_n[i], T_parts_n[i+1]) for i in range(len(T_parts_n)-1)]
-Nparts_n = len(intervals_n)
-
-
-def get_interval_T(T: float, intervals: list = intervals) -> int:
+def get_interval_T(T: float) -> int:
     """ The intervals are treated as half-open on the higher side i.e., [a, b)"""
     for i in range(Nparts):
         if intervals[i][0] <= T <= intervals[i][1]:  # this returns the first interval it belongs to unless
@@ -24,7 +18,6 @@ def get_interval_T(T: float, intervals: list = intervals) -> int:
 
 if __name__ == '__main__':
     Ti = 8.95
-    inters = intervals
-    interval_num = get_interval_T(Ti, inters)
-    print("Intervals: ", inters)
-    print("Interval of Ti={} is ".format(Ti) + str(interval_num) + ": " + str(inters[interval_num]))
+    interval_num = get_interval_T(Ti)
+    print("Intervals: ", intervals)
+    print("Interval of Ti={} is ".format(Ti) + str(interval_num) + ": " + str(intervals[interval_num]))
