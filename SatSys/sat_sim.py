6aaa49e5140c6c6d05237e45bc696bd41adac058
scharla <scharla@purdue.edu>
2025-02-17 14:47:01 -0500

    working on the simulation.

--- SatSys/sat_sim.py
+++ SatSys/sat_sim.py
@@ -11,7 +11,22 @@ class sat_sim:
         """ Loads the data and generates the simulation of the saturated system """
         self.dat = dec_dat
         self.theta_sat = ths.theta_sat(self.dat)
+        self.eta_sim = self.sim_eta()
+        self.data_len = self.theta_sat.cAb.data_len
     # ===============================================================================
 
-    def phi_k(self):
+    def phi_sat(self, k) -> np.ndarray:
         """ Calculates the phi(k) for getting the eta(k+1) """
+        u1_k = self.dat.ssd['u1'][k]
+        F_k = self.dat.ssd['F'][k]
+        T_k = self.dat.ssd['T'][k]
+        phi_k = phiT.phi_T(T_k)
+        return (u1_k/F_k)*phi_k
+    # ============================================================
+
+    def sim_eta(self):
+        """ Simulate the eta from data """
+        eta_sim = np.zeros(self.data_len)
+        for k in range(self.data_len):
+
+
