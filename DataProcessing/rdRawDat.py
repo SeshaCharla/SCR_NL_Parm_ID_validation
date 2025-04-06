e9cad5795a86a70dc84f8e84802c5682766154e1
Seshu <scharla@purdue.edu>
2025-04-05 22:33:14 -0400

    made the raw data file working.

--- DataProcessing/rdRawDat.py
+++ DataProcessing/rdRawDat.py
@@ -12,6 +12,7 @@ class RawTestData():
         """Reads the test data and stores into a .raw dictionary"""
         self.dt = 0.2           # Sampling time
         self.name = self.test_name(age, test_type)
+        # print(self.name)
         self.dat_file = self.data_dir()
         self.raw = self.load_test_data()
 
@@ -24,36 +25,50 @@ class RawTestData():
         # Assigning the Data to the variables
         # Time is in seconds
         raw_data['t'] = np.array(data.get(('LOG_TM', 'sec')), dtype=np.float64).flatten()
+        # print("len(t) = ", np.shape(raw_data['t']))
         # ======================================================================================
         # Temperature is in deg-C
         Tin = np.array(data.get(('V_AIM_TRC_DPF_OUT', 'Deg_C')), dtype=np.float64).flatten()
         Tout = np.array(data.get(('V_AIM_TRC_SCR_OUT', 'Deg_C')), dtype=np.float64).flatten()
         Tscr = np.mean([Tin, Tout], axis=0).flatten()
         raw_data['T'] = uc.uConv(Tscr, Tscr, "-T0C")
+        # print("len(T) = ", np.shape(raw_data['T']))
         # ======================================================================================
         # Mass flow rate is in g/sec
         F_kgmin = np.array(data.get(('EXHAUST_FLOW', 'kg/min')), dtype=np.float64).flatten()
         raw_data['F'] = uc.uConv(F_kgmin,Tscr, "kg/min to 10 g/s")        # g/sec
+        # print("len(F) = ", np.shape(raw_data['F']))
         # =======================================================================================
         # NOx output is in mol/m^3
         NOx = np.array(data.get(('EXH_CW_NOX_COR_U1', 'PPM')), dtype=np.float64).flatten()
+        if len(NOx) != len(raw_data['t']):   # For the new data set
+            NOx = np.array(data.get(('EXH_CW_NOX_FTIR_COR_U2', 'PPM')), dtype=np.float64).flatten()
         raw_data['x1'] = uc.uConv(NOx, Tscr, "ppm to 10^-3 mol/m^3")
+        # print("len(x1) = ", np.shape(raw_data['x1']))
         # =======================================================================================
         # NH3 output is in mol/m^3
         NH3 = np.array(data.get(('EXH_CW_AMMONIA_MEA', 'ppm')), dtype=np.float64).flatten()
         raw_data['x2'] = uc.uConv(NH3, Tscr, "ppm to 10^-3 mol/m^3")
+        # print("len(x2) = ", np.shape(raw_data['x2']))
         # =======================================================================================
         # NOx out measured in mol/m^3
         y1 = np.array(data.get(('V_SCM_PPM_SCR_OUT_NOX', 'ppm')), dtype=np.float64).flatten()
+        if len(y1) != len(raw_data['t']):       # For the new data-set
+            y1 = np.array(data.get(('V_SCM_PPM_SCR_OUT_NOX_PRECLAMP', 'ppm')), dtype=np.float64).flatten()
         raw_data['y1'] = uc.uConv(y1, Tscr, "ppm to 10^-3 mol/m^3")
+        # print("len(y1) = ", np.shape(raw_data['y1']))
         # =======================================================================================
         # NOx input is in mol/m^3
         u1 = np.array(data.get(('ENG_CW_NOX_FTIR_COR_U2', 'PPM')), dtype=np.float64).flatten()
+        if len(u1) != len(raw_data['t']):
+            u1 = np.array(data.get(('EONOX_COMP_VALUE', 'ppm')), dtype=np.float64).flatten()
         raw_data['u1'] = uc.uConv(u1, Tscr, "ppm to 10^-3 mol/m^3")
+        # print("len(u1) = ", np.shape(raw_data['u1']))
         # ========================================================================================
         # Urea injection rate is in ml/sec
         u2 = np.array(data.get(('V_UIM_FLM_ESTUREAINJRATE', 'ml/sec')), dtype=np.float64).flatten()
         raw_data['u2'] = uc.uConv(u2, Tscr, "ml/s to 10^-1 ml/s")
+        # print("len(u2) = ", np.shape(raw_data['u2']))
         # u1_sensor = np.array(Data.get(('EONOX_COMP_VALUE', 'ppm'))).flatten()
         # ======================================================================================================
         return raw_data
@@ -100,7 +115,8 @@ class RawTestData():
 ## =====================================================================================================================
 def load_test_data_set():
     # Load the test Data
-    test_data = [[RawTestData(age, tst) for tst in range(3)] for age in range(2)]
+    ag_tsts = [12, 3]
+    test_data = [[RawTestData(age, tst) for tst in range(ag_tsts[age])] for age in range(2)]
     return test_data
 
 # ======================================================================================================================
@@ -110,10 +126,11 @@ if __name__ == "__main__":
     # Actually load the entire Data set ----------------------------------------
     test_data = load_test_data_set()
     fig_dpi = 300
+    ag_tsts = [12, 3]
 
     # Plotting all the Data sets
     for i in range(2):
-        for j in range(3):
+        for j in range(ag_tsts[i]):
             for key in ['u1', 'u2', 'T', 'F', 'x1', 'x2', 'y1']:
                 plt.figure()
                 plt.plot(test_data[i][j].raw['t'], test_data[i][j].raw[key], label=test_data[i][j].name + " " + key, linewidth=1)
